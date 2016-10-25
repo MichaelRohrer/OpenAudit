74da82399d76f9f856fa01727da766747d216e7a
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2016-10-25 08:54:56 +0200

    Authentication and authorization boilerplate validated

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'code'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo/code-development',
+    jwtsecret: process.env.JWTSECRET || 'supersecretsharedkey'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'code'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo/code-test',
+    jwtsecret: process.env.JWTSECRET || 'supersecretsharedkey'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'code'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo/code-production',
+    jwtsecret: process.env.JWTSECRET || 'supersecretsharedkey'
+  }
+};
+
+module.exports = config[env];
