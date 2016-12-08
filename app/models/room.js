3a97dffc9a968ccad3edfeac42e56573b83c4efb
Thomas_Hernandez <thomas.hernandez@heig-vd.ch>
2016-12-08 14:16:21 +0100

    first commit

--- app/models/room.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var RoomSchema = new Schema({
-    name: { type: String, required: true, index: { unique: true }},
-    owner: { type: String, required: true }
-});
-
-mongoose.model('Room', RoomSchema);
